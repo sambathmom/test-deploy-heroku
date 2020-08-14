e9f4fba8bfd0c17bacc916b7f5057f53d709ba33
sambathmom <sambathmom06@gmail.com>
2020-08-14 13:36:33 +0700

    first commit

---
+++ src/test/java/com/springboot/testdeploy/TestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.testdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
